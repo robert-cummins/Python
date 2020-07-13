eb00cf40b6b25b117d4b5a113e78461d9b353528
robert-cummins <robert.k.cummins@gmail.com>
2020-07-13 17:20:14 +1200

    update weight converter

--- PythonBasics/weightConverter.py
+++ PythonBasics/weightConverter.py
@@ -1,3 +1,10 @@
-weight_in_pounds = input('What is your weight in pounds? ')
-convert_to_kg = int(weight_in_pounds) / 2.2
-print('Your weight in KGs is ' + str(convert_to_kg))
+weight = input('What is your weight? ')
+weight_type = input('Is that (l)bs or (K)Gs ')
+convert_to_kg = int(weight) / 2.2
+convert_to_pounds = int(weight) * 2.2
+
+if weight_type.upper() == 'L':
+    print('Your weight in KGs is ' + str(convert_to_kg))
+elif weight_type.upper() == 'K':
+    print('Your weight in lbs is ' + str(convert_to_pounds))
+
