0bfaba2e9417b226ec9e626e00d421efa917a56b
robert-cummins <robert.k.cummins@gmail.com>
2020-07-16 17:45:00 +1200

    learn python functions with simple example

---
+++ PythonBasics/functions.py
@@ -0,0 +1,9 @@
+def greet_user(first_name, last_name):
+    print(f"Hi there {first_name} {last_name}")
+    print("Welcome!")
+
+
+print("start")
+greet_user("Rob", "Cummins")
+greet_user("Beth", "Anslow")
+print("finish")
