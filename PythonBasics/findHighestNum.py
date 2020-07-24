f7ba02fe2ff9e57497b7c521b91c42ee74e030e4
robert-cummins <robert.k.cummins@gmail.com>
2020-07-24 17:05:21 +1200

    simple modules example

--- PythonBasics/findHighestNum.py
+++ PythonBasics/findHighestNum.py
@@ -1,8 +1,8 @@
-numbers = [2, 6, 1, 20, 15, 17, 23, 7, 12]
-highest_number = numbers[0]
+def find_highest_num(list):
+    highest = 0
+    for num in list:
+        if num > highest:
+            highest = num
+    print(highest)
 
-for num in numbers:
-    if num > highest_number:
-        highest_number = num
 
-print(highest_number)
---
+++ PythonBasics/modules.py
@@ -0,0 +1,5 @@
+import findHighestNum
+
+numbers = [2, 4, 7, 34, 3, 7, 56, 34, 53, 21, 43]
+
+findHighestNum.find_highest_num(numbers)
